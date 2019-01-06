2ff53098a3d7562bb2f49c961b9ce2a5611ab688
DidierMatos <didiermat00s@gmail.com>
2019-01-06 02:54:10 -0500

    Cambio de descripción y nombre de proyecto, debido a que se reutilizo la base de un proyecto subido a GitHub anteriormente

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Plantillas-Curriculum-Vitae
-Plantillas para Curriculum Vitae realizadas en Vue Cli 3 WebPack
+Plantillas para Curriculum Vitae que se iran desarrollando con el tiempo en Vue Cli 3 WebPack
--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,9 @@
 <template>
   <v-app class="main">
-    <v-toolbar dark color="primary">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-  
-      <v-toolbar-title class="white--text"><v-icon>format_list_bulleted</v-icon> Aplicación web traductor de texto a Morse y viceversa</v-toolbar-title>
-  
-      <v-spacer></v-spacer>
-  
-      <!-- <v-btn icon>
-        <v-icon>search</v-icon>
-      </v-btn>
-  
-      <v-btn icon>
-        <v-icon>apps</v-icon>
-      </v-btn>
-  
-      <v-btn icon>
-        <v-icon>refresh</v-icon>
-      </v-btn> -->
-  
-      <v-btn icon>
-        <v-icon>more_vert</v-icon>
-      </v-btn>
 
-    </v-toolbar>
 
     <v-content>
-      <traductor/>
+      <PlantillaCV1/>
     </v-content>
   </v-app>
 </template>
